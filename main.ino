cfbc31de93e5fef217b1d5b6b58c87e7a82594f1
vitor <vitor.iwagoe@gmail.com>
2026-06-09 19:03:42 -0300

    3

--- main.ino
+++ main.ino
@@ -63,4 +63,52 @@ void loop() {
       updateSignalLEDs();
     }
   }
+  delay(50);
+}
+
+void readSensors() {
+  temperature += random(-50, 51) / 100.0;
+  humidity += random(-100, 101) / 100.0;
+
+  if (temperature < 10.0) temperature = 10.0;
+  if (temperature > 40.0) temperature = 40.0;
+  if (humidity < 30.0) humidity = 30.0;
+  if (humidity > 90.0) humidity = 90.0;
+
+  Serial.print("T: ");
+  Serial.print(temperature);
+  Serial.print(" H: ");
+  Serial.println(humidity);
+
+  sendData("SENSOR");
+}
+
+void updateGPS() {
+  float lat = -23.5505 + (random(-100, 101) / 10000.0);
+  float lon = -46.6333 + (random(-100, 101) / 10000.0);
+  currentGPS = String(lat, 4) + ", " + String(lon, 4);
+
+  Serial.print("GPS: ");
+  Serial.println(currentGPS);
+
+  sendData("GPS");
+}
+
+void sendData(String dataType) {
+  String status = cellularSignal ? "CELULAR" : "SATELITE";
+
+  String dataPacket = "{\"type\": \"" + dataType + "\", ";
+  dataPacket += "\"timestamp\": " + String(millis()) + ", ";
+  dataPacket += "\"gps\": \"" + currentGPS + "\", ";
+  dataPacket += "\"temp\": " + String(temperature, 1) + ", ";
+  dataPacket += "\"hum\": " + String(humidity, 1) + ", ";
+  dataPacket += "\"panic\": " + String(panicActive ? "true" : "false") + ", ";
+  dataPacket += "\"signal\": \"" + status + "\"}";
+
+  Serial.println(dataPacket);
+}
+
+void updateSignalLEDs() {
+  digitalWrite(ledCellularPin, cellularSignal ? HIGH : LOW);
+  digitalWrite(ledSatellitePin, cellularSignal ? LOW : HIGH);
 }
